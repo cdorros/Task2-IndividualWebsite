85fbffb954a923e9e9a97ba85766477e6c32bd27
Chris Dorros <chris.dorros@sv.cmu.edu>
2011-09-24 17:43:08 -0400

    Inital commit of individual website. Starting with ruby tutorial progess.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '34c8a120dd73f9c40b397031a020bf9130edb17aebeb16881eae0b5f270e9ea4b30eae992d7e5ade850e6d101ba1078177998bbcc63ea6ab76965b1e086bfa42'
