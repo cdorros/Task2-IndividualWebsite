459d8867d50c36221cfe6642d14e5c5fcb382af5
Chris Dorros <chris.dorros@sv.cmu.edu>
2011-09-24 20:04:04 -0400

    Finished adding user stuff (for now)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
 
+  resources :users
+
 #  resources :users
 
 #  get "pages/home"
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110924001902) do
+ActiveRecord::Schema.define(:version => 20110924232850) do
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
